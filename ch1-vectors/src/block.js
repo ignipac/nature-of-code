8ffb76ddb21de40780ae055bddb9062d6c83d7ba
ignipac <63119806+ignipac@users.noreply.github.com>
2026-09-08 08:21:18 +0100

    auto commit Sep 08, 2026, 08:21:18
    modified: ch1-vectors/src/block.js
    modified: ch1-vectors/src/sketch.js

--- ch1-vectors/src/block.js
+++ ch1-vectors/src/block.js
@@ -1,23 +1,24 @@
 export class Block {
-  constructor(p5, size) {
+  constructor(p5, position, size) {
     this.p = p5
-    this.posX = this.p.width/2;
-    this.posY = this.p.height/2;
+    this.pos = position
     this.angle = 0;
     this.size = size;
     this.maxDistance = 50;
   }
 
   display() {
-    this.p.translate(this.posX, this.posY)
+    this.p.push(); // understand why I need to push & pop?
+    this.p.translate(this.pos.x, this.pos.y)
     this.p.rotate(this.angle)
     this.p.rect(0, 0, this.size, this.size)
+    this.p.pop();
   }
 
   move() {
-    let distanceFromMouse = this.p.dist(this.p.mouseX, this.p.mouseY, this.posX, this.posY)
+    let distanceFromMouse = this.p.dist(this.p.mouseX, this.p.mouseY, this.pos.x, this.pos.y)
     if (distanceFromMouse < this.maxDistance) {
-      this.angle += this.p.PI / 128;
+      this.angle += 1; //degrees
     }
   }
 }
--- ch1-vectors/src/sketch.js
+++ ch1-vectors/src/sketch.js
@@ -4,70 +4,61 @@ import { Block } from './block';
 const sketches = document.getElementById('sketches')
 
 new p5((p) => { // p - processing in-built functions
+  let cols; let rows; let cellSize = 50;
+
   // Objects
-  let block;
+  let blocks = [];
   let ball;
 
   p.setup = () => {
     p.createCanvas(400, 400)
     p.rectMode(p.CENTER)
-    // p.angleMode(p.DEGREES)
+    p.angleMode(p.DEGREES)
 
-    // Assign Objects
-    block = {
-      posX: p.width / 2,
-      posY: p.height / 2,
-      angle: 0,
-      size: 50,
-      maxDistance: 100,
+    rows = p.width / cellSize
+    cols = p.height / cellSize
 
-      display: function () {
-        p.translate(this.posX, this.posY)
-        p.rotate(this.angle)
-        p.rect(0, 0, this.size, this.size)
-      },
-
-      move: function () {
-        let distanceFromMouse = p.dist(p.mouseX, p.mouseY, this.posX, this.posY)
-        if (distanceFromMouse < this.maxDistance) {
-          this.angle += p.PI / 128;
-        }
+    for (let i = 0; i < cols; i++) {
+      blocks[i] = [];
+      for (let j = 0; j < rows; j++){
+        blocks[i][j] = new Block(p, p.createVector(cellSize/2 + i*cellSize, cellSize/2 + j*cellSize), cellSize)
       }
     }
 
     ball = {
-      posX: p.width / 2,
-      posY: p.height / 2,
+      pos: p.createVector(p.width / 2, p.height / 2),
+      speed: p.createVector(2, 2.5),
       size: 50,
-      speedX: 2,
-      speedY: 2.5,
       display: function () {
         // p.translate(this.posX, this.posY)
-        p.circle(this.posX, this.posY, this.size)
+        p.circle(this.pos.x, this.pos.y, this.size)
         p.fill('grey')
         p.stroke('black')
         p.strokeWeight(2)
       },
       bounce: function () {
-        this.posX += this.speedX
-        this.posY += this.speedY
-
-        if (this.posX > p.width - this.size/2 || this.posX < 0 + this.size/2) {
-          this.speedX *= -1
+        this.pos.x += this.speed.x
+        this.pos.y += this.speed.y
+        if (this.pos.x > p.width - this.size/2 || this.pos.x < 0 + this.size/2) {
+          this.speed.x *= -1
         }
-        if (this.posY > p.height - this.size/2 || this.posY < 0 + this.size/2) {
-          this.speedY *= -1
+        if (this.pos.y > p.height - this.size/2 || this.pos.y < 0 + this.size/2) {
+          this.speed.y *= -1
         }
       }
     }
-
-
   }
 
   p.draw = () => {
     p.background('white') // clear previous draw
-    ball.display()
-    ball.bounce()
+    // ball.display()
+    // ball.bounce()
 
+    for (const colBlocks of blocks) {
+      for (const cell of colBlocks) {
+        cell.display()
+        cell.move()
+      }
+    }
   };
 }, sketches)
